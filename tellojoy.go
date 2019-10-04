f751718c13d897d577ad2b2df92460185ce48e12
Dev Bharel <dev@spacemanholdings.com>
2019-10-04 14:03:17 -0500

    inital commit

---
+++ tellojoy.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/DanielFallon/gobot/platforms/dji/tello"
+	"gobot.io/x/gobot"
+)
+
+func main() {
+	// port 8889 is the one on which it recieves instructions, 8888 is where it replies
+	setupJoystickConfig()
+	drone0 := tello.NewEDUDriver("192.168.8.196:8889", "8888")
+	work := func() {
+		//drone0.TakeOff()
+		droneCommand(0, drone0)
+	}
+	racer0 := gobot.NewRobot("Racer1",
+		[]gobot.Connection{},
+		[]gobot.Device{drone0},
+		work,
+	)
+
+	racer0.Start()
+}
+
+//func droneCommand(drone, cmd){}
